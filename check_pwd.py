114ec2e7943588db5eb0278757f800ac408e6e2e
Sariah Bunnell <bunnells@flip2.engr.oregonstate.edu>
2020-10-26 19:12:05 -0700

    changed check_pwd to return false if no numbers in string, test 6 passed

--- check_pwd.py
+++ check_pwd.py
@@ -6,6 +6,13 @@ def check_pwd(password):
         # has uppercase return True
         if password.islower():
             return False
+        # has digit
+        no_digit = True
+        for character in password:
+            if character.isdigit():
+                no_digit = False
+        if no_digit:
+            return False        
         return True
     return False
 
