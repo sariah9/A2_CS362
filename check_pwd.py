0e979c17de3acc22cc6fc2a373532996cb62e6dd
Sariah Bunnell <bunnells@flip2.engr.oregonstate.edu>
2020-10-26 18:34:37 -0700

    first test, input as empty string

---
+++ check_pwd.py
@@ -0,0 +1,3 @@
+def check_pwd(password):
+    return True
+
---
+++ tests.py
@@ -0,0 +1,18 @@
+import unittest
+from check_pwd import check_pwd
+
+
+# Sariah Bunnell
+# October 26 2020
+# Passwords are 8 - 20 characters
+# 1+ lowercase, 1+ uppercase
+# 1+ digit, 1+ symbol [~`!@#$%^&*()_+-=]
+
+
+class TestDrivenDevelopment(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
