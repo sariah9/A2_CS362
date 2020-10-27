8c28472f234e67dda845a3882ff5f83c0bd4ad38
Sariah Bunnell <bunnells@flip2.engr.oregonstate.edu>
2020-10-26 19:00:28 -0700

    changed check_pwd to return false if no lowercase letters, test 4 passed

--- check_pwd.py
+++ check_pwd.py
@@ -1,5 +1,8 @@
 def check_pwd(password):
     if 8 <= len(password) <= 20:
+        # has lowercase return True
+        if password.isupper():
+            return False
         return True
     return False
 
